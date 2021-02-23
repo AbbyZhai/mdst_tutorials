fd368227b49c26a3311e0ffd1642bd5ff51c4d6c
AbbyZhai <50309878+AbbyZhai@users.noreply.github.com>
2021-02-22 19:33:15 -0800

    completed checkpont1b

--- checkpoint1/checkpoint1b.py
+++ checkpoint1/checkpoint1b.py
@@ -7,31 +7,52 @@ Here you will create a script to preprocess the data given in starbucks.csv. You
 a jupyter notebook or python terminal to develop your code and test each function as you go... 
 you can import this file and its functions directly:
 
-    - jupyter notebook: include the lines `%autoreload 2` and `import preprocess`
-                        then just call preprocess.remove_percents(df) to test
+    - jupyter notebook: include the lines `%autoreload 2` and `import checkpoint1b`
+                        then just call checkpoint1b.remove_percents(df) to test
                         
-    - python terminal: run `from importlib import reload` and `import preprocess`
-                       each time you modify this file, run `reload(preprocess)`
+    - python terminal: run `from importlib import reload` and `import checkpoint1b`
+                       each time you modify this file, run `reload(checkpoint1b)`
 
 Once you are finished with this program, you should run `python checkpoint1b.py` from the terminal.
 This should load the data, perform preprocessing, and save the output to the data folder.
 
 """
 import pandas as pd
+import numpy as np
 
 def remove_percents(df, col):
+    # if any nan in df[col], fill nan to 0%
+    if df[col].isnull().any():
+        df[col].fillna('0%', inplace=True)
+    # remove all the % char at the end of the str 
+    df[col] = df[col].apply(lambda x: x[:-1])
+    # convert str # to numneric float and to int
+    df[col] = df[col].apply(pd.to_numeric).astype(int)
+    
     return df
 
 def fill_zero_iron(df):
+    df['Iron (% DV)'] = df['Iron (% DV)'].fillna(0)
     return df
     
 def fix_caffeine(df):
+    df['Caffeine (mg)'].replace(['Varies', 'varies'], np.nan, inplace=True)
+    mean_value = df['Caffeine (mg)'].median()
+    df['Caffeine (mg)'].fillna(value=mean_value, inplace=True)
+    df['Caffeine (mg)'] = df['Caffeine (mg)'].astype(int)
     return df
 
 def standardize_names(df):
+    df.columns = df.columns.str.lower()
+    df.columns = df.columns.str.replace(' \([a-zA-Z %]+\)', '')
     return df
 
 def fix_strings(df, col):
+    df[col] = df[col].str.lower()
+    # remove all the ® chars
+    #df[col] = df[col].str.replace('®', '')
+    # remove all the non-alpha chars
+    df[col] = df[col].str.replace(r'[^A-Za-z]', '')
     return df
 
 
